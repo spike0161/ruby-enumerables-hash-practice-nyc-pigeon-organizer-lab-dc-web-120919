7d8160eb81cdc664f018f4a0929412753302a05f
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-10-18 14:25:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,9 +7,9 @@ def nyc_pigeon_organizer(data)
     
     value.each do |name|
       # If pigeon name exists in solution, add the color to their colors, else add a new hash for that pigeon
-       
+        binding.pry
       if solution[name]
-       binding.pry
+      
         solution[name.to_sym][:color] << key.to_s
       else
         solution[name.to_sym] = {
